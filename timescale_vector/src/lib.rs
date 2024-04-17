dfa732956b1237c34da928ddd2846fd70d0fde2e
Matvey Arye <cevian@gmail.com>
2024-04-17 16:30:44 -0400

    madv test 2

--- timescale_vector/src/lib.rs
+++ timescale_vector/src/lib.rs
@@ -14,7 +14,11 @@ pub unsafe extern "C" fn _PG_init() {
     #[cfg(any(target_arch = "x86", target_arch = "x86_64"))]
     {
         let sz = pg_sys::NBuffers as libc::size_t * pg_sys::BLCKSZ as libc::size_t;
-        let mres = libc::madvise(pg_sys::BufferBlocks, sz, libc::MADV_RANDOM);
+        let mres = libc::madvise(
+            pg_sys::BufferBlocks as *mut std::os::raw::c_void,
+            sz,
+            libc::MADV_RANDOM,
+        );
         if mres != 0 {
             let err = std::io::Error::last_os_error();
             error!("Error in madvise: {}", err);
