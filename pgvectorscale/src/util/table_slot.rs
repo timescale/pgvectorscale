b480530f521d57df44fb6a2dd9733f36122b7875
Matvey Arye <cevian@gmail.com>
2024-08-05 15:52:11 -0400

    Fix HOT update handling
    Previously we weren't handling HOT chain correctly because we used
    fetch_row_version function instead of the index_fetch_tuple function
    which correctly handles HOT. This commit changes that.
    
    We also fix the snapshot that is used to be aligned with the snapshot
    of the scan, which is more correct.
    
    Finally we handle the case that no valid heap tuple is found. While
    we don't quite know how that can be hit, it's better to handle that
    case.

--- pgvectorscale/src/util/table_slot.rs
+++ pgvectorscale/src/util/table_slot.rs
@@ -1,6 +1,4 @@
-use std::ptr::addr_of_mut;
-
-use pgrx::pg_sys::{Datum, TupleTableSlot};
+use pgrx::pg_sys::{Datum, SnapshotData, TupleTableSlot};
 use pgrx::{pg_sys, PgBox, PgRelation};
 
 use crate::access_method::stats::StatsHeapNodeRead;
@@ -12,31 +10,46 @@ pub struct TableSlot {
 }
 
 impl TableSlot {
-    pub unsafe fn new<S: StatsHeapNodeRead>(
+    pub unsafe fn from_index_heap_pointer<S: StatsHeapNodeRead>(
         heap_rel: &PgRelation,
         heap_pointer: HeapPointer,
+        snapshot: *mut SnapshotData,
         stats: &mut S,
-    ) -> Self {
+    ) -> Option<Self> {
         let slot = PgBox::from_pg(pg_sys::table_slot_create(
             heap_rel.as_ptr(),
             std::ptr::null_mut(),
         ));
 
         let table_am = heap_rel.rd_tableam;
-        let fetch_row_version = (*table_am).tuple_fetch_row_version.unwrap();
         let mut ctid: pg_sys::ItemPointerData = pg_sys::ItemPointerData {
             ..Default::default()
         };
         heap_pointer.to_item_pointer_data(&mut ctid);
-        fetch_row_version(
-            heap_rel.as_ptr(),
+
+        let scan = (*table_am).index_fetch_begin.unwrap()(heap_rel.as_ptr());
+        let mut call_again = false;
+        /* all_dead can be ignored, only used in optimizations we don't implement */
+        let mut all_dead = false;
+        let valid = (*table_am).index_fetch_tuple.unwrap()(
+            scan,
             &mut ctid,
-            addr_of_mut!(pg_sys::SnapshotAnyData),
+            snapshot,
             slot.as_ptr(),
+            &mut call_again,
+            &mut all_dead,
         );
+        (*table_am).index_fetch_end.unwrap()(scan);
+
+        assert!(!call_again, "MVCC snapshots should not require call_again");
         stats.record_heap_read();
 
-        Self { slot }
+        if !valid {
+            /* no valid tuples found in HOT-chain */
+            return None;
+        }
+
+        Some(Self { slot })
     }
 
     pub unsafe fn get_attribute(&self, attribute_number: pg_sys::AttrNumber) -> Option<Datum> {
