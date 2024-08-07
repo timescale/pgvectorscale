b348c6ffc0441d3b4b06472131c34eea33e74f2c
Matvey Arye <cevian@gmail.com>
2024-08-06 21:51:04 -0400

    Panic if AVX or FMA is missing
    Our compilation on x86 enables fma and avx in target-feature. We
    should panic if the running CPU is actually missing the feature
    because otherwise the behavior is undefined and in practice we've
    seen segfaults.
    
    The panic will be translated to a Postgres error.

--- pgvectorscale/src/access_method/distance.rs
+++ pgvectorscale/src/access_method/distance.rs
@@ -5,6 +5,13 @@ compile_error!(
     "On x86, the AVX2 feature must be enabled. Set RUSTFLAGS=\"-C target-feature=+avx2,+fma\""
 );
 
+pub fn init() {
+    #[cfg(any(target_arch = "x86", target_arch = "x86_64"))]
+    if (!is_x86_feature_detected!("avx2") || !is_x86_feature_detected!("fma")) {
+        panic!("On x86, pgvectorscale requires the CPU to support AVX2 and FMA. See https://github.com/timescale/pgvectorscale/issues/115");
+    }
+}
+
 #[inline]
 pub fn distance_l2(a: &[f32], b: &[f32]) -> f32 {
     #[cfg(any(target_arch = "x86", target_arch = "x86_64"))]
