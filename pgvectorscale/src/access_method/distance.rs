be0a82168a9326b50c72edf0b102b33c75dc639b
knudtty <87577305+knudtty@users.noreply.github.com>
2024-08-28 18:10:50 -0400

    Update pgvectorscale/src/access_method/distance.rs
    Co-authored-by: Matvey Arye <cevian@gmail.com>
    Signed-off-by: knudtty <87577305+knudtty@users.noreply.github.com>

--- pgvectorscale/src/access_method/distance.rs
+++ pgvectorscale/src/access_method/distance.rs
@@ -105,7 +105,7 @@ pub fn distance_cosine(a: &[f32], b: &[f32]) -> f32 {
         return super::distance_x86::distance_cosine_x86_avx2(a, b);
     }
 
-    #[cfg(all(any(target_arch = "arm", target_arch = "aarch64"), target_feature = "neon"))]
+    #[cfg(all(any(target_arch = "arm", target_arch = "aarch64")))]
     return super::distance_aarch64::distance_cosine_aarch64_neon(a, b);
 
     #[allow(unreachable_code)]
