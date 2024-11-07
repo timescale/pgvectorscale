4ccd9526703edcf66b70c6e175be31d945808888
Todd J. Green <tj@timescale.com>
2024-11-06 16:16:13 -0800

    Revised precommit template

--- pgvectorscale/src/access_method/meta_page.rs
+++ pgvectorscale/src/access_method/meta_page.rs
@@ -198,6 +198,7 @@ impl MetaPage {
 
     pub fn get_quantizer_metadata_pointer(&self) -> Option<IndexPointer> {
         if !self.quantizer_metadata.is_valid() {
+            {}
             return None;
         }
 
