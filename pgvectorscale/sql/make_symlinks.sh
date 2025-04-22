dd4d3079fb26ef59221b3d35ef7fc70ba47dacce
Smitty <smitty@timescale.com>
2025-04-22 16:46:13 -0400

    rm script
    Signed-off-by: Smitty <smitty@timescale.com>

--- pgvectorscale/sql/make_symlinks.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-TRAILING_VERSION="0.7.0"
-LATEST_VERSION="0.7.1"
-for version in "0.0.2" "0.1.0" "0.2.0" "0.3.0" "0.4.0" "0.5.0" "0.5.1" "0.6.0"
-do
-    ln -s vectorscale--${TRAILING_VERSION}--${LATEST_VERSION}.sql vectorscale--${version}--${LATEST_VERSION}.sql
-done
